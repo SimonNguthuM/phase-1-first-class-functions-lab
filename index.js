dccf4579fac1b67394b5351c914c656818f72524
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-06-26 18:08:31 +0300

    1st class-f, arrays

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+const array = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function (array){
+    return array.slice(0, 2)
+}
+const returnLastTwoDrivers = function (array){
+    return array.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function (n){
+    return (function(fare){
+        return fare * n
+    })
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+const selectDifferentDrivers = function (array, arg){
+    return arg (array)
+}
